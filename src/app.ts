eafec232e6df73ba2b5cc14873d8b314498f3ef9
situ2001 <yongcong2001@outlook.com>
2022-08-01 16:20:01 +0800

    style: pretty src下的文件

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import { Component } from 'react'
-import './app.scss'
+import { Component } from 'react';
+import './app.scss';
 
 class App extends Component {
+  componentDidMount() {}
 
-  componentDidMount () {}
+  componentDidShow() {}
 
-  componentDidShow () {}
-
-  componentDidHide () {}
+  componentDidHide() {}
 
   // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
+  render() {
+    return this.props.children;
   }
 }
 
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,23 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.scss'
+import { Component } from 'react';
+import { View, Text } from '@tarojs/components';
+import './index.scss';
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
       </View>
-    )
+    );
   }
 }
