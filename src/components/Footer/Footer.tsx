a182f7c1ea5ee1541ec8b06d7d5924ab058687e1
favorhau <1057072764@qq.com>
2022-08-04 13:28:17 +0800

    fix: 增加footer上下边距

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ import GreyLogo from '../../images/bin/logo-grey.svg';
  */
 export default function Footer() {
   return (
-    <View className='flex flex-col items-center justify-center'>
+    <View className='flex flex-col items-center justify-center mt-4 mb-4'>
       <View className='flex flex-row'>
         <Image style='width: 24px;height: 24px;' src={GreyLogo} />
         <Text className='logo-text'>羊谷</Text>
