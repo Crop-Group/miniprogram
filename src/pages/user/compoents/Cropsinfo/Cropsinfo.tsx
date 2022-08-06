e2f53750dac2753a9295d143a39ac109153c1bb1
favorhau <1057072764@qq.com>
2022-08-06 22:08:39 +0800

    refactor: 新增用户页作物收藏数量组件

--- src/pages/user/compoents/Cropsinfo/Cropsinfo.tsx
+++ src/pages/user/compoents/Cropsinfo/Cropsinfo.tsx
@@ -1,4 +1,5 @@
 import { View, Image, Text } from '@tarojs/components';
+import ScanLogo from '../../../../images/res/user/menu/scan.svg';
 import './CropsInfo.scss';
 
 interface cropsProps {
@@ -14,15 +15,17 @@ export default function CropsInfo(props: cropsProps) {
   const { collectionNum, cropsNum } = props;
 
   return (
-    <View className='flex flex-row items-start w-full' style='padding-left: 25px; width: 80%'>
-      <View className='flex flex-col'>
-        <Text> {collectionNum} </Text>
-        <Text> 我的作物 </Text>
-      </View>
-      <Image src=''></Image>
-      <View className='flex flex-col'>
-        <Text> {cropsNum} </Text>
-        <Text> 我的收藏 </Text>
+    <View className='w-full flex flex-col justify-center items-center py-4'>
+      <View className='flex flex-row items-center bg-white container justify-center' style='width: 90%'>
+        <View className='crops-container flex flex-col'>
+          <Text className='num flex flex-row items-center justify-center'> {collectionNum} </Text>
+          <Text className='description'> 我的作物 </Text>
+        </View>
+        <Image className='logo-scan' src={ScanLogo}></Image>
+        <View className='collection-container flex flex-col'>
+          <Text className='num flex flex-row items-center justify-center'> {cropsNum} </Text>
+          <Text className='description'> 我的收藏 </Text>
+        </View>
       </View>
     </View>
   );
