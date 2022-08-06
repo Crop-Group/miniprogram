85e05e706b260cd992ca91274407235c106df7ea
favorhau <1057072764@qq.com>
2022-08-06 23:30:51 +0800

    refactor: 将footer部分图标文字设置水平方向居中

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,7 +12,7 @@ import GreyLogo from '../../images/bin/logo-grey.svg';
 export default function Footer() {
   return (
     <View className='flex flex-col items-center justify-center mt-4 mb-4'>
-      <View className='flex flex-row'>
+      <View className='flex flex-row justify-center items-center'>
         <Image style='width: 24px;height: 24px;' src={GreyLogo} />
         <Text className='logo-text'>羊谷</Text>
       </View>
