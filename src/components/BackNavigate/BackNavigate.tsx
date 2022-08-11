ddfc9ccb432cb3249883d0006968cf375f0d7596
situ2001 <yongcong2001@outlook.com>
2022-08-12 00:05:57 +0800

    fix: 返回栏返回键不与菜单💊居中

--- src/components/BackNavigate/BackNavigate.tsx
+++ src/components/BackNavigate/BackNavigate.tsx
@@ -1,6 +1,5 @@
 import { View } from '@tarojs/components';
 import Taro from '@tarojs/taro';
-import './BackNavigate.scss';
 
 interface BackNavigateConfig {
   title?: string;
@@ -14,7 +13,7 @@ interface BackNavigateConfig {
  *
  * title optional 导航标题
  *
- * title required 黑夜模式 打开则标题栏白色 否则为黑色
+ * darkMode required 黑夜模式 打开则标题栏白色 否则为黑色
  */
 export default function BackNavigate(props: BackNavigateConfig) {
   const { title, darkMode } = props;
@@ -29,12 +28,16 @@ export default function BackNavigate(props: BackNavigateConfig) {
     }
   };
   return (
-    <View className='flex flex-row justify-between' style={{ color: darkMode ? 'white' : 'black' }}>
-      <View className='logo-back flex items-end h-full justify-center' onClick={() => navigateBack()}>
-        {'<'}
+    <View className='flex flex-row justify-between h-full' style={{ color: darkMode ? 'white' : 'black' }}>
+      <View
+        className='w-2/10 flex items-center h-full justify-center'
+        style={{ fontSize: '2rem' }}
+        onClick={() => navigateBack()}
+      >
+        &#8249;
       </View>
       <View className='title-navigate'>{title}</View>
-      <View className='blank'></View>
+      <View className='w-2/10'></View>
     </View>
   );
 }
