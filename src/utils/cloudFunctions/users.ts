a964c229aa9bc9dd41ff15fc88f9b12d556b2e70
favorhau <1057072764@qq.com>
2022-08-12 20:07:08 +0800

    refactor: 封装users相关函数，初始化用户、用户登录

--- src/utils/cloudFunctions/users.ts
+++ src/utils/cloudFunctions/users.ts
@@ -1,9 +1,10 @@
 import Taro from '@tarojs/taro';
-import { CloudFunctionsResult } from '../../../types/functions';
+import { LoginResultPromise, InitUserPromise } from '../../../types/functions';
 
 /**
  * @description
  * 用户登录
+ * @param 无请求参数
  * @example
  * const handleLogin = async () => {
         const _ = await login()
@@ -11,15 +12,15 @@ import { CloudFunctionsResult } from '../../../types/functions';
         // {
         //   status: 1,  
         //   result: {
-        //     nickName: '',
-        //     avatarUrl: '',
-        //     ...
+        //     nickName: 'nickName',
+        //     avatarUrl: 'avatarUrl',
+        //     userID: '*****'.
         // }
         //}
     }
  */
 
-const login = async (): Promise<CloudFunctionsResult> => {
+const login = async (): LoginResultPromise => {
   try {
     const _ = await Taro.getUserProfile({
       desc: '获取用户头像、昵称，作物数据',
@@ -36,11 +37,11 @@ const login = async (): Promise<CloudFunctionsResult> => {
         },
       },
     });
-    __.result = typeof __.result === 'string' ? {} : __.result;
+    __.result = (typeof __.result === 'string' ? {} : __.result) ?? {};
     return {
       status: 1,
       result: {
-        ...__.result,
+        userID: __.result.userID,
         nickName,
         avatarUrl,
       },
@@ -49,10 +50,74 @@ const login = async (): Promise<CloudFunctionsResult> => {
   } catch (e) {
     return {
       status: 0,
-      result: {},
       errMsg: e,
     };
   }
 };
 
-export { login };
+/**
+ * @description
+ * 初始化用户
+ * @param 无请求参数
+ * @example
+ * const handleInitUser = async () => {
+        const _ = await initUser()
+        console.log(_)
+        // {
+        //   status: 1,  
+        //   result: {
+        //     nickName: 'nickName',
+        //     avatarUrl: 'avatarUrl',
+        //     userID: '*****'.
+        // }
+        //}
+    }
+ */
+
+const initUser = async (): InitUserPromise => {
+  try {
+    const _ = await Taro.getUserProfile({
+      desc: '获取用户头像、昵称，作物数据',
+    });
+    const nickName = _.userInfo['nickName'];
+    const avatarUrl = _.userInfo['avatarUrl'];
+
+    const __ = await Taro.cloud.callFunction({
+      name: 'tempFunctions',
+      data: {
+        type: 'users',
+        data: {
+          nickName: nickName,
+        },
+      },
+    });
+    const ___ = await Taro.cloud.callFunction({
+      name: 'tempFunctions',
+      data: {
+        type: 'initUser',
+        data: {
+          nickName: nickName,
+        },
+      },
+    });
+
+    ___.result = (typeof ___.result === 'string' ? {} : ___.result) ?? {};
+
+    return {
+      status: 1,
+      result: {
+        ...___.result,
+        nickName,
+        avatarUrl,
+      },
+      errMsg: ___.errMsg,
+    };
+  } catch (e) {
+    return {
+      status: 0,
+      errMsg: e,
+    };
+  }
+};
+
+export { login, initUser };
