c152efc31423a3949affacee25ce4cf25f9dc75d
favorhau <1057072764@qq.com>
2022-08-12 22:10:38 +0800

    refactor: 完成收藏云函数封装

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,13 @@
 import { login, initUser } from './cloudFunctions/users';
 import { getNews } from './cloudFunctions/news';
-import { getCollections } from './cloudFunctions/collections';
+import {
+  getCollections,
+  getSingleCollection,
+  addSingleCollection,
+  removeSingleCollection,
+} from './cloudFunctions/collections';
 
 /**
- * 1
+ * 输出封装接口
  */
-export { login, getNews, initUser, getCollections };
+export { login, getNews, initUser, getCollections, getSingleCollection, addSingleCollection, removeSingleCollection };
