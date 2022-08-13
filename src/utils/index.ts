278aed2ffda343cdb14cf8d0ff15072637eea3d5
favorhau <1057072764@qq.com>
2022-08-13 19:21:58 +0800

    refactor: 完成云函数封装

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,8 +6,33 @@ import {
   addSingleCollection,
   removeSingleCollection,
 } from './cloudFunctions/collections';
+import {
+  getCrops,
+  getSingleCrop,
+  addSingleCrop,
+  addSingleCropLog,
+  deleteSingleCrop,
+  finishSingleCrop,
+  findNearCrops,
+} from './cloudFunctions/crops';
 
 /**
  * 输出封装接口
+ * 一共
  */
-export { login, getNews, initUser, getCollections, getSingleCollection, addSingleCollection, removeSingleCollection };
+export {
+  login,
+  getNews,
+  initUser,
+  getCollections,
+  getSingleCollection,
+  addSingleCollection,
+  removeSingleCollection,
+  getCrops,
+  getSingleCrop,
+  addSingleCrop,
+  addSingleCropLog,
+  deleteSingleCrop,
+  finishSingleCrop,
+  findNearCrops,
+};
