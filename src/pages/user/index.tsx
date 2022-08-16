6b8d1918e589f2c59aec9736debf0994f0ea3f05
situ2001 <yongcong2001@outlook.com>
2022-08-16 23:46:05 +0800

    feat(user/index.tsx): 判断是否登录

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,5 +1,5 @@
 import { View } from '@tarojs/components';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useDidShow } from '@tarojs/taro';
 import './index.scss';
@@ -8,16 +8,27 @@ import { HomeBackGround } from '../../components/Background';
 import { UserInfo } from './components/UserInfo';
 import { CropsInfo } from './components/CropsInfo';
 import { Menu } from './components/Menu';
-import { CropsCollectionStoreContext, CropsStoreContext } from '../../store/providers';
+import { CropsCollectionStoreContext, CropsStoreContext, UserStoreContext } from '../../store/providers';
 
 function User() {
   const cropsStore = useContext(CropsStoreContext);
   const collectionStore = useContext(CropsCollectionStoreContext);
+  const userStore = useContext(UserStoreContext);
+
+  useEffect(() => {
+    if (userStore.isLogin) {
+      console.log('已经登录');
+      cropsStore.fetch();
+      collectionStore.fetch();
+    }
+  }, [userStore.isLogin, cropsStore, collectionStore]);
 
   // 发起查询收藏作物请求
   useDidShow(() => {
-    cropsStore.fetch();
-    collectionStore.fetch();
+    if (userStore.isLogin) {
+      cropsStore.fetch();
+      collectionStore.fetch();
+    }
   });
 
   return (
@@ -28,7 +39,10 @@ function User() {
       showFooter
     >
       <UserInfo />
-      <CropsInfo collectionNum={collectionStore.length} cropsNum={cropsStore.length}></CropsInfo>
+      <CropsInfo
+        collectionNum={userStore.isLogin ? collectionStore.length : 0}
+        cropsNum={userStore.isLogin ? cropsStore.length : 0}
+      ></CropsInfo>
       <Menu />
     </Layout>
   );
