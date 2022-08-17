e6a88a25dde1f22772ca591b6a5b860192685050
favorhau <1057072764@qq.com>
2022-08-17 23:35:40 +0800

    feat: 新增windcss样式

--- windi.config.js
+++ windi.config.js
@@ -7,5 +7,16 @@ export default {
   corePlugins: {
     // 禁用掉在小程序环境中不可能用到的 plugins
     container: false
-  }
+  },
+  theme: {
+    textColor: {
+      primary: '#34A853',
+      white: '#FFFFFF'
+    },
+    backgroundColor: theme => ({
+        ...theme('colors'),
+        primary: '#34A853',
+        secondary: '#A5D1D4'
+    }),
+  },
 }
