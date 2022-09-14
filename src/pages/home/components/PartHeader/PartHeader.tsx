d26210951659f298a2c401a2c37442ea6dacb48e
favorhau <1057072764@qq.com>
2022-09-14 23:47:46 +0800

    fix: 修正partHeader分割组件的色彩设定

--- src/pages/home/components/PartHeader/PartHeader.tsx
+++ src/pages/home/components/PartHeader/PartHeader.tsx
@@ -13,8 +13,8 @@ const PartHeader = (props: lineInfo) => {
     <View className='flex flex-col w-full mt-10 mb-8'>
       <View className='font-bold flex flex-col ml-12'>
         <Text className='text-bold text-4xl ml-4'>{title}</Text>
-        <View className='bg-primary h-2 w-20 rounded-4xl mt-4 mr-4 ml-12 z-100'></View>
-        <View className='grey-line ml-2' style={'background:' + { tabColor }}></View>
+        <View className='h-2 w-20 rounded-4xl mt-4 mr-4 ml-12 z-100' style={'background:' + tabColor}></View>
+        <View className='grey-line ml-2'></View>
       </View>
     </View>
   );
