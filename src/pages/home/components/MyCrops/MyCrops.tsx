d99a165ca67f261e53a72d2880691eb5a777bbdb
favorhau <1057072764@qq.com>
2022-09-14 23:34:36 +0800

    feat: 添加我的羊谷页

--- src/pages/home/components/MyCrops/MyCrops.tsx
+++ src/pages/home/components/MyCrops/MyCrops.tsx
@@ -7,7 +7,7 @@ import { userStore, cropsStore } from '../../../../store/providers';
 
 const MyCrops = () => {
   const [autoPlay, setAutoPlay] = useState(true);
-
+  const [currentPage, setCurrentPage] = useState(0);
   useEffect(() => {
     if (userStore.isLogin) {
       console.log('MyCrops:', 'useEffect trigger');
@@ -20,7 +20,15 @@ const MyCrops = () => {
       <PartHeader title='我的羊谷' tabColor='#34A853'></PartHeader>
       <View className='w-full flex flex-row'>
         <View className='ml-12'>
-          <Swiper className='h-78 w-78 rounded-6xl -z-0' autoplay={autoPlay}>
+          <Swiper
+            className='h-78 w-78 rounded-6xl -z-0'
+            style={!cropsStore.crops.length ? 'background: #49B967' : ''}
+            autoplay={autoPlay}
+            current={currentPage}
+            onChange={(e) => {
+              setCurrentPage(e.detail.current);
+            }}
+          >
             {cropsStore.crops.map((e) => {
               return (
                 <SwiperItem className='rounded-6xl'>
@@ -31,15 +39,30 @@ const MyCrops = () => {
           </Swiper>
           <View className='flex flex-col justify-between h-78 -mt-78 text-white'>
             <View className='flex flex-col z-100 p-6'>
-              <Text className='text-xl py-1'>提供生产全程追溯</Text>
-              <Text className='text-5xl font-bold py-1'>{cropsStore.length}棵</Text>
+              <View className='flex flex-row'>
+                <View className='flex flex-col'>
+                  <Text className='text-xl py-1'>提供生产全程追溯</Text>
+                  <Text className='text-5xl font-bold py-1'>{cropsStore.length}棵</Text>
+                </View>
+                <Image className='w-22 h-22 pl-8 pt-2' src={require('../../../../images/res/index/seed.svg')}></Image>
+              </View>
               <Text className='font-bold py-1'>作物正在托管中</Text>
             </View>
             <View
-              className='bg-black h-22 w-full rounded-6xl z-100 opacity-80 flex flex-row justify-between items-center'
+              className='bg-black h-24 w-full rounded-6xl z-100 opacity-80 flex flex-row justify-between items-center'
               style='background: rgba(109,114,120,0.49)'
             >
-              <Image className='w-10 h-10 ml-6' src={require('../../../../images/res/index/last.svg')}>
+              <Image
+                className='w-10 h-10 ml-6'
+                src={require('../../../../images/res/index/last.svg')}
+                onClick={() => {
+                  setCurrentPage(currentPage - 1 ? currentPage : 0);
+                  // setTimeout(()=>{
+                  //     console.log(currentPage)
+                  //     setCurrentPage(currentPage+1)
+                  // }, 500)
+                }}
+              >
                 {' '}
               </Image>
               <Image
@@ -51,7 +74,15 @@ const MyCrops = () => {
                 }
                 onClick={() => [setAutoPlay(!autoPlay)]}
               ></Image>
-              <Image className='w-10 h-10 mr-6' src={require('../../../../images/res/index/next.svg')}>
+              <Image
+                className='w-10 h-10 mr-6'
+                src={require('../../../../images/res/index/next.svg')}
+                onClick={() => {
+                  setCurrentPage(
+                    currentPage + 1 > cropsStore.crops.length - 1 ? cropsStore.crops.length - 1 : currentPage + 1,
+                  );
+                }}
+              >
                 {' '}
               </Image>
             </View>
@@ -60,20 +91,20 @@ const MyCrops = () => {
 
         <View className='flex flex-col h-78 justify-between ml-6'>
           <View
-            className='bg-black rounded-6xl w-78 h-36 flex items-center justify-center'
+            className='bg-black rounded-6xl w-78 h-36 flex items-center justify-center manage-button'
             style='background: #44D7B6'
             onClick={() => {}}
           >
-            <Image className='w-20 h-20' src={require('../../../../images/res/index/plus.svg')} />
-            <Text className='text-4xl text-white p-3 text-bold'>新增作物</Text>
+            <Image className='w-22 h-22' src={require('../../../../images/res/index/manage.svg')} />
+            <Text className='font-2xl text-white p-4 text-bold '>新增作物</Text>
           </View>
           <View
-            className='bg-black rounded-6xl w-78 h-36 flex items-center justify-center'
+            className='bg-black rounded-6xl w-78 h-36 flex items-center justify-center plus-button'
             style='background: #0091FF'
             onClick={() => {}}
           >
-            <Image className='w-22 h-22' src={require('../../../../images/res/index/manage.svg')} />
-            <Text className='text-4xl text-white p-3 text-bold'>追加记录</Text>
+            <Image className='w-20 h-20' src={require('../../../../images/res/index/plus.svg')} />
+            <Text className='font-2xl text-white p-4 text-bold'>追加记录</Text>
           </View>
         </View>
       </View>
