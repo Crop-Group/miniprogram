440936609da724e15e7311e7f2c0366309346893
favorhau <favorhau@outlook.com>
2022-09-22 09:21:30 +0800

    fix: 修改partHeader样式

--- src/pages/home/components/PartHeader/PartHeader.tsx
+++ src/pages/home/components/PartHeader/PartHeader.tsx
@@ -1,5 +1,4 @@
 import { View, Text } from '@tarojs/components';
-
 import './index.scss';
 
 interface lineInfo {
@@ -11,7 +10,7 @@ const PartHeader = (props: lineInfo) => {
   const { title, tabColor } = props;
   return (
     <View className='flex flex-col w-full mt-10 mb-8'>
-      <View className='font-bold flex flex-col ml-12'>
+      <View className='font-bold flex flex-col'>
         <Text className='text-bold text-4xl ml-4'>{title}</Text>
         <View className='h-2 w-20 rounded-4xl mt-4 mr-4 ml-12 z-100' style={'background:' + tabColor}></View>
         <View className='grey-line ml-2'></View>
